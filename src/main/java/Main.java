5aa316427f3c82655bacbfdaca0ed3946a6439d5
i4ata <i.rusinov@student.rug.nl>
2022-11-24 12:39:06 +0100

    first commit, hello world + expert

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Expert name: Persi Pencheva");
+        System.out.println("Field of expertise: Pharmacy (lifelong experience in Bulgaria)");
+    }
+}
