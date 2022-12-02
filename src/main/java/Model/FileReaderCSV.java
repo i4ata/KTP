830bf83527b7f7ebbabb44f5610d5bec70ac19a6
i4ata <i.rusinov@student.rug.nl>
2022-12-02 22:29:53 +0100

    did reading of csv files. Bit bad but works

--- src/main/java/Model/FileReaderCSV.java
+++ src/main/java/Model/FileReaderCSV.java
@@ -1,5 +1,22 @@
 package Model;
 
+import java.util.*;
+
 public class FileReaderCSV {
-    //TODO: Figure out how to read the csvs.
+
+    public static Map<String, String[]> readKB(String category) {
+        Map<String, String[]> medicineSpecs = new HashMap<>();
+        try (Scanner fileInput = new Scanner(
+                FileReaderCSV.class.getResourceAsStream("/KnowledgeBase/" + category + "Medicine.csv"))) {
+            while (fileInput.hasNextLine()) {
+                String line = fileInput.nextLine();
+                String[] parts = line.split(",");
+                medicineSpecs.put(parts[0], Arrays.copyOfRange(parts, 1, parts.length));
+            }
+        }
+        return medicineSpecs;
+    }
+    public static void main(String[] args) {
+        System.out.println(readKB("ThroatPain"));
+    }
 }
